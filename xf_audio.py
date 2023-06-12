bbb3c639d9beb2369362c3a164adbf0bbe3227db
1ighter <zjplightert@icloud.com>
2023-06-12 18:00:24 +0800

    fix bug

--- xf_audio.py
+++ xf_audio.py
@@ -3,16 +3,15 @@ import base64
 import hashlib
 import hmac
 import json
-import ssl
-import sys
 import time
+import xmltodict
 from datetime import datetime
 from time import mktime
 from urllib.parse import urlencode
 from wsgiref.handlers import format_date_time
 
 import websocket
-import xmltodict
+
 
 
 class WebsocketReq(object):
@@ -100,19 +99,9 @@ class WebsocketReq(object):
             data = data.decode('utf-8')
             dic = xmltodict.parse(data)
             dic = json.dumps(dic, indent=4)
+            # print(dic)
             dic2 = json.loads(dic)
-
-            f = open("daneil.txt","w")
-            f.write(dic)
-            f.close()
-            self.result['句子精准度'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['@accuracy_score']
-            self.result['句子内容'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['@content']
-            self.result['句子流畅度'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['fluency_score']
-            self.result['句子标准度'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['@standard_score']
-            self.result['句子完整度'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['@integrity_score']
-            self.result['句子总得分'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['@total_score']
-            self.result['单词'] = data['xml_result']['read_sentence']['rec_paper']['read_chapter']['word']
-            print(self.result['单词'])
+            self.result['data'] = dic2
             time.sleep(1)
             self.ws.close()
             #print("thread terminating...")
@@ -162,18 +151,28 @@ class WebsocketReq(object):
         # ret = websocket_instance.pop(ws)
         # return ret
 
-def test1():
+def test():
 
+    reftext = """
+[choice]
+1. my name is jack
+2. i am jack
+3. How should I deal with this subject hand?
+[keywords]
+my name is | i am | hand
+    """
+
+    a = time.time()
     WebsocketReq1 = WebsocketReq(
         appid='5f20ecf1',
         apisecret='ea49d6aca557c416e7e2fd8f66ac98b6',
         apikey='e0b76912f87ee883e5be737b6d83681b',
-        audio_file=r'D:\xf\学生语音测试音频\01 Week 1 Lesson 1 Speaking - Daniel Vocabulary.wav',
-        text='good morning,good night,good afternoon,good evening,name,fine',
-        category='read_sentence',
+        audio_file=r'C:\Users\14348\Desktop\head.wav',
+        text=reftext,
+        category='simple_expression',
         ent='en_vip')
     websocket.enableTrace(False)
     data = WebsocketReq1.upload()
     print(data)
 
-test1()
+test()
